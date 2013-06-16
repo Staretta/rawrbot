0746b3d896506ab32f23b4de8ad28b243cc43f80
Staretta Hawk <staretta.hawk@gmail.com>
2013-06-16 15:28:05 -0600

    Updated RateLimiter.java

--- RawrBot/src/java/net/a1337ism/modules/RateLimiter.java
+++ RawrBot/src/java/net/a1337ism/modules/RateLimiter.java
@@ -15,12 +15,13 @@ import org.pircbotx.hooks.ListenerAdapter;
 
 public class RateLimiter extends ListenerAdapter {
     // Set up the logger stuff
-    private static Logger            logger       = LogManager.getFormatterLogger(RawrBot.class);
-    private static Marker            LOG_EVENT    = MarkerManager.getMarker("LOG_EVENT");
+    private static Logger                     logger       = LogManager.getFormatterLogger(RawrBot.class);
+    private static Marker                     LOG_EVENT    = MarkerManager.getMarker("LOG_EVENT");
 
-    private static int               timeout      = 60000;                                       // Milliseconds 600000
-    private static int               maxRequests  = 5;
-    private static Map<String, List> userRequests = new HashMap<String, List>();
+    private static int                        timeout      = 60000;                                       // Milliseconds
+                                                                                                           // 600000
+    private static int                        maxRequests  = 5;
+    private static volatile Map<String, List> userRequests = new HashMap<String, List>();
 
     // userRequests is our hashmap of users and how many times they do a command.
     // Basically something like { username : [ millisec1, millisec2 ] } Where username is the key, and the list is the
@@ -29,11 +30,14 @@ public class RateLimiter extends ListenerAdapter {
     private static void addRequest(String nickname) {
         // If the hashmap contains the username as a key
         if (userRequests.containsKey(nickname)) {
-            // Make a list, and put that user's requests into the list.
-            List<Long> timeList = userRequests.get(nickname);
+            synchronized (RateLimiter.class) {
+                // Make a list, and put that user's requests into the list.
+                List<Long> timeList = userRequests.get(nickname);
 
-            timeList.add(System.currentTimeMillis());
-            userRequests.put(nickname, timeList);
+                timeList.add(System.currentTimeMillis());
+
+                userRequests.put(nickname, timeList);
+            }
         } else {
             // If the user is not in the hashmap, then add them to the hashmap along with the current time
             List<Long> timeList = new ArrayList<Long>();
@@ -48,26 +52,17 @@ public class RateLimiter extends ListenerAdapter {
 
         // If the hashmap contains the nickname
         if (userRequests.containsKey(nickname)) {
-            // Make a list, and put that user's requests into the list.
-            List<Long> timeList = userRequests.get(nickname);
+            synchronized (RateLimiter.class) {
+                // Make a list, and put that user's requests into the list.
+                List<Long> timeList = userRequests.get(nickname);
 
-            if (timeList.size() < maxRequests) {
-                addRequest(nickname);
-                return false;
-            } else {
-                return true;
+                if (timeList.size() < maxRequests) {
+                    addRequest(nickname);
+                    return false;
+                } else {
+                    return true;
+                }
             }
-            // If the list size is greater than max requests, then return true, because they are currently at the
-            // command limit.
-            // if (timeList.size() > maxRequests) {
-            // return true;
-            // } else {
-            // // If the list size is less than maxRequests, then we know they aren't limited, and need to add the
-            // // current time to the list.
-            // addRequest(nickname);
-            // // We then return false, because they aren't rate limited at the moment.
-            // return false;
-            // }
         } else {
             // If the user is not in the hashmap, then we know cleanupRequest deleted their old requests.
             addRequest(nickname);
@@ -79,24 +74,29 @@ public class RateLimiter extends ListenerAdapter {
     private static void cleanupRequest(String nickname) {
         // If the hashmap contains the nickname
         if (userRequests.containsKey(nickname)) {
-            // Make a list, and put that user's requests into the list.
-            List<Long> timeList = userRequests.get(nickname);
-            int listSize = timeList.size();
 
-            // We need to try and see if any of the times listed in the list can be removed, and then put it into the
-            // hashmap if we removed anything from the list.
-            for (int i = timeList.size() - 1; i >= 0; i--) {
-                // If the time is less than the current system time minus timeout, then remove that time from the list
-                if (timeList.get(i) < (System.currentTimeMillis() - timeout)) {
-                    timeList.remove(i);
+            synchronized (RateLimiter.class) {
+                // Make a list, and put that user's requests into the list.
+                List<Long> timeList = userRequests.get(nickname);
+                int listSize = timeList.size();
+
+                // We need to try and see if any of the times listed in the list can be removed, and then put it into
+                // the
+                // hashmap if we removed anything from the list.
+                for (int i = timeList.size() - 1; i >= 0; i--) {
+                    // If the time is less than the current system time minus timeout, then remove that time from the
+                    // list
+                    if (timeList.get(i) < (System.currentTimeMillis() - timeout)) {
+                        timeList.remove(i);
+                    }
                 }
-            }
-            if (timeList.isEmpty()) {
-                // If the list is empty, then we remove the name from the hashmap.
-                userRequests.remove(nickname);
-            } else if (timeList.size() < listSize) {
-                // If the list is less than it's original size, then put it back into the hashmap.
-                userRequests.put(nickname, timeList);
+                if (timeList.isEmpty()) {
+                    // If the list is empty, then we remove the name from the hashmap.
+                    userRequests.remove(nickname);
+                } else if (timeList.size() < listSize) {
+                    // If the list is less than it's original size, then put it back into the hashmap.
+                    userRequests.put(nickname, timeList);
+                }
             }
         }
     }
