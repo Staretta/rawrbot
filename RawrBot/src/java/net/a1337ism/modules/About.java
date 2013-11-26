5a6f8d1e121c04850512047d0ec90f56db9c8108
Staretta Hawk <staretta.hawk@gmail.com>
2013-11-25 18:34:22 -0700

    Added an about command. Unfinished atm,

---
+++ RawrBot/src/java/net/a1337ism/modules/About.java
@@ -0,0 +1,24 @@
+package net.a1337ism.modules;
+
+import net.a1337ism.RawrBot;
+
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.events.PrivateMessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class About extends ListenerAdapter {
+    private static Logger logger = LoggerFactory.getLogger(RawrBot.class);
+
+    public void onMessage(MessageEvent event) throws Exception {
+        if (event.getMessage().trim().toLowerCase().startsWith("!about")
+                && !RateLimiter.isRateLimited(event.getUser().getNick())) {
+
+        }
+    }
+
+    public void onPrivateMessage(PrivateMessageEvent event) throws Exception {
+
+    }
+}
