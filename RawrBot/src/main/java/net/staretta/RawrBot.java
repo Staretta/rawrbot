f4870ba938b5fddaf10f2be7e5d64a8590ab1621
Staretta Hawk <Staretta.Hawk@gmail.com>
2014-05-30 02:42:42 -0600

    Added SSL Support for servers. Added MultiBotManager, to take care of starting multiple bots. Added Youtube.java and Vimeo.java. Initial add of RawrBotUtil.java. It needs more work. Removed BoneCP dependencies from pom.xml.

--- RawrBot/src/main/java/net/staretta/RawrBot.java
+++ RawrBot/src/main/java/net/staretta/RawrBot.java
@@ -1,22 +1,17 @@
 package net.staretta;
 
-import java.io.IOException;
 import java.util.List;
 
 import net.staretta.businesslogic.entity.Settings;
 import net.staretta.businesslogic.services.SettingsService;
-import net.staretta.util.ircUtil;
 
 import org.pircbotx.Configuration;
 import org.pircbotx.Configuration.Builder;
+import org.pircbotx.MultiBotManager;
 import org.pircbotx.PircBotX;
-import org.pircbotx.exception.IrcException;
+import org.pircbotx.UtilSSLSocketFactory;
 import org.pircbotx.hooks.Listener;
 import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.events.NickAlreadyInUseEvent;
-import org.pircbotx.hooks.events.PrivateMessageEvent;
-import org.pircbotx.hooks.events.QuitEvent;
-import org.pircbotx.hooks.events.UnknownEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -24,103 +19,57 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class RawrBot extends ListenerAdapter implements Listener
 {
-	// slf4j Stuff
-	private static Logger logger = LoggerFactory.getLogger(RawrBot.class);
-	private static SettingsService settingsService;
-	private String botOwner;
-	private String ircChannel;
-	private String botNickname;
-	private String botPassword;
-	
-	@Override
-	public void onPrivateMessage(PrivateMessageEvent event) throws Exception
+	public static void main(String[] args)
 	{
-		if ((event.getUser().getNick().equalsIgnoreCase(botOwner) || ircUtil.isOP(event, ircChannel))
-				&& event.getMessage().equalsIgnoreCase("!quit"))
-		{
-			// Shutdown upon receiving a quit command
-			ircUtil.sendMessage(event, "Shutting Down...");
-			event.getBot().stopBotReconnect();
-			event.getBot().sendIRC().quitServer();
-		}
-		else if (event.getUser().getNick().equalsIgnoreCase(botOwner) && event.getMessage().equalsIgnoreCase("!join"))
-		{
-			String[] param = event.getMessage().trim().split("\\s", 3);
-			
-			if (param.length != 1)
-				event.getBot().sendIRC().joinChannel(param[1]);
-		}
-	}
-	
-	@Override
-	public void onQuit(QuitEvent event) throws Exception
-	{
-		// If we see our default nickname quit, then rename our name to it.
-		if (event.getUser().getNick().equalsIgnoreCase(botNickname))
-		{
-			event.getBot().sendIRC().changeNick(botNickname);
-		}
-	}
-	
-	@Override
-	public void onNickAlreadyInUse(NickAlreadyInUseEvent event)
-	{
-		event.respond(alterCollidedNick(event.getUsedNick()));
-	}
-	
-	private String alterCollidedNick(String nickname)
-	{
-		// If there is already a nickname with our name on the server, then we need to change our name so it'll work.
-		if (nickname.contains("Rawr"))
-		{
-			nickname.replace("Rawr", "Rawrr");
-			return nickname;
-		}
-		else
-		{
-			return nickname + "_";
-		}
-	}
-	
-	@Override
-	public void onUnknown(UnknownEvent event) throws Exception
-	{
-		logger.info(event.toString());
-	}
-	
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException,
-			IrcException
-	{
+		Logger logger = LoggerFactory.getLogger(RawrBot.class);
+
 		logger.info("Initializing Spring context.");
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/application-context.xml");
 		logger.info("Spring context initialized.");
-		
+
 		SettingsService settingsService = (SettingsService) applicationContext.getBean(SettingsService.class);
-		List<Settings> settings = settingsService.getBotSettings();
-		
-		PircBotX bot;
-		for (Settings s : settings)
+		List<Settings> serverSettings = settingsService.getBotSettings();
+		logger.info("Bot Settings Loaded.");
+
+		MultiBotManager<PircBotX> manager = new MultiBotManager();
+		for (Settings setting : serverSettings)
 		{
 			// @formatter:off
 			Builder builder = new Configuration.Builder()
-				.setName(s.getNickname())
-				.setLogin(s.getUsername())
-				.setRealName(s.getVersion())
+				.setName(setting.getNickname())
+				.setLogin(setting.getUsername())
+				.setRealName(setting.getVersion())
 				.setAutoNickChange(true)
 				.setAutoReconnect(true)
 				.setCapEnabled(true)
 				.setIdentServerEnabled(false)
-				.setServerHostname(s.getServer())
-				.setServerPort(s.getPort())
-				.setNickservPassword(s.getPassword());
+				.setServerHostname(setting.getServer())
+				.setServerPort(setting.getPort())
+				.setNickservPassword(setting.getPassword());
 			// @formatter:on
-			for (String channel : s.getChannels())
+			if (setting.isSsl())
+				builder.setSocketFactory(new UtilSSLSocketFactory().trustAllCertificates());
+
+			for (String channel : setting.getChannels())
 				builder.addAutoJoinChannel(channel);
-			for (String module : s.getModules())
-				builder.addListener((Listener) Class.forName(module).newInstance());
+
+			for (String module : setting.getModules())
+			{
+				try
+				{
+					builder.addListener((Listener) Class.forName("net.staretta.modules." + module).newInstance());
+				}
+				catch (InstantiationException | IllegalAccessException | ClassNotFoundException e)
+				{
+					logger.error("Exception in RawrBot.main ", e);
+				}
+			}
+
 			Configuration config = builder.buildConfiguration();
-			bot = new PircBotX(config);
-			bot.startBot();
+			manager.addBot(config);
+			logger.info("Added IRC bot to manager: " + setting.getServer());
 		}
+		logger.info("Starting IRC bots.");
+		manager.start();
 	}
 }
---
+++ RawrBot/src/main/java/net/staretta/modules/RawrBotUtil.java
@@ -0,0 +1,56 @@
+package net.staretta.modules;
+
+import net.staretta.util.ircUtil;
+
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.PrivateMessageEvent;
+import org.pircbotx.hooks.events.QuitEvent;
+import org.pircbotx.hooks.events.UnknownEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RawrBotUtil extends ListenerAdapter
+{
+	private Logger	logger	= LoggerFactory.getLogger(RawrBotUtil.class);
+	private String	botOwner;
+	private String	ircChannel;
+	private String	botNickname;
+	private String	botPassword;
+
+	@Override
+	public void onPrivateMessage(PrivateMessageEvent event) throws Exception
+	{
+		if ((event.getUser().getNick().equalsIgnoreCase(botOwner) || ircUtil.isOP(event, ircChannel))
+				&& event.getMessage().equalsIgnoreCase("!quit"))
+		{
+			// Shutdown upon receiving a quit command
+			ircUtil.sendMessage(event, "Shutting Down...");
+			event.getBot().stopBotReconnect();
+			event.getBot().sendIRC().quitServer();
+		}
+		else if (event.getUser().getNick().equalsIgnoreCase(botOwner) && event.getMessage().equalsIgnoreCase("!join"))
+		{
+			String[] param = event.getMessage().trim().split("\\s", 3);
+
+			if (param.length != 1)
+				event.getBot().sendIRC().joinChannel(param[1]);
+		}
+	}
+
+	@Override
+	public void onQuit(QuitEvent event) throws Exception
+	{
+		// If we see our default nickname quit, then rename our name to it.
+
+		if (event.getUser().getNick().equalsIgnoreCase(botNickname))
+		{
+			event.getBot().sendIRC().changeNick(botNickname);
+		}
+	}
+
+	@Override
+	public void onUnknown(UnknownEvent event) throws Exception
+	{
+		logger.debug(event.toString());
+	}
+}
