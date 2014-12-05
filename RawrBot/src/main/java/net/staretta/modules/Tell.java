c4b7cd369d38c3380ec379cd961ca6d17db0df5c
Staretta <Staretta.Hawk@gmail.com>
2014-12-04 18:08:19 -0700

    Done with first revision of Tell Command. Need to add on message as well, as per dew's recommendation.

--- RawrBot/src/main/java/net/staretta/modules/Tell.java
+++ RawrBot/src/main/java/net/staretta/modules/Tell.java
@@ -2,11 +2,7 @@ package net.staretta.modules;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
 import net.staretta.RawrBot;
 import net.staretta.businesslogic.BaseListener;
 import net.staretta.businesslogic.ModuleInfo;
@@ -17,16 +13,20 @@ import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.events.JoinEvent;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.PrivateMessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Tell extends BaseListener
 {
 	TellService service;
-	
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	public Tell()
 	{
 		service = RawrBot.applicationContext.getBean(TellService.class);
 	}
-	
+
 	@Override
 	protected ModuleInfo setModuleInfo()
 	{
@@ -36,21 +36,22 @@ public class Tell extends BaseListener
 		moduleInfo.setVersion("v1.0");
 		moduleInfo.addCommand("!tell", "!tell <Nickname> <Message> : Tells the user the message, "
 				+ "if they are offline, they will be notified of the message when they join channel.");
-		moduleInfo.addCommand("!told",
-				"!told [Nickname] : Displays messages to a user, or all users, and if a user has received the messages");
+		moduleInfo
+				.addCommand("!told",
+						"!told [Nickname] : Displays messages to a user, or all users, and if a user has received the messages");
 		// moduleInfo.addCommand("!note", "!note <Nickname> : Gives a note to a user");
 		return moduleInfo;
 	}
-	
+
 	@Override
 	public void onJoin(JoinEvent<PircBotX> event) throws Exception
 	{
 		super.onJoin(event);
-		
+
 		if (!event.getUser().getNick().equals(event.getBot().getNick()))
 		{
-			ArrayList<TellEntity> tells = service
-					.getTells(event.getUser().getNick(), event.getBot().getConfiguration().getServerHostname());
+			ArrayList<TellEntity> tells = service.getTells(event.getUser().getNick(), event.getBot().getConfiguration()
+					.getServerHostname());
 			if (tells != null)
 			{
 				for (TellEntity tell : tells)
@@ -58,29 +59,30 @@ public class Tell extends BaseListener
 					String date = "[" + new SimpleDateFormat("MM/dd/yy HH:mm:ss").format(tell.getDate()) + "]";
 					String message = date + " <" + tell.getFromNickname() + "> " + tell.getMessage();
 					event.getUser().send().notice(message);
-					
+
 					tell.setTold(true);
 					service.setTold(tell);
 				}
 			}
 		}
 	}
-	
+
 	@Override
 	public void OnMessage(MessageEvent event)
 	{
 		if (isCommand(event.getMessage(), "!tell"))
 		{
-			ArrayList<String> params = new ArrayList<String>(Arrays.asList(event.getMessage().split("\\s")));
-			if (params.size() > 2)
+			String[] params = event.getMessage().replace("!tell", "").trim().split("\\s");
+			if (params.length > 1)
 			{
 				StringBuilder sb = new StringBuilder();
-				for (int i = 2; i < params.size(); i++)
-					sb.append(params.get(i) + " ");
+				for (int i = 1; i < params.length; i++)
+					sb.append(params[i] + " ");
 				String message = sb.toString().trim();
-				String toNickname = params.get(1);
-				
-				if (service.addTell(event.getUser(), toNickname, message, event.getBot().getConfiguration().getServerHostname()))
+				String toNickname = params[0];
+
+				if (service.addTell(event.getUser(), toNickname, message, event.getBot().getConfiguration()
+						.getServerHostname()))
 					event.getChannel().send().message(toNickname + " will be told: " + message);
 			}
 			else
@@ -90,45 +92,49 @@ public class Tell extends BaseListener
 		}
 		else if (isCommand(event.getMessage(), "!told"))
 		{
-			ArrayList<String> params = new ArrayList<String>(Arrays.asList(event.getMessage().split("\\s")));
-			if (params.size() > 1)
+			String[] params = event.getMessage().replaceFirst("!told", "").trim().split("\\s");
+			ArrayList<TellEntity> tolds;
+			if (params[0].equals("-a") || params[0].equals("-all") || params[0].equals(""))
 			{
-				String toNickname = params.get(1);
-				
-				ArrayList<TellEntity> tolds = service.getTolds(event.getUser().getNick(), toNickname, event.getBot().getConfiguration()
+				tolds = service.getAllTolds(event.getUser().getNick(), event.getBot().getConfiguration()
 						.getServerHostname());
-				if (tolds != null)
-				{
-					for (TellEntity told : tolds)
-					{
-						String message = told.isTold() + " [" + new SimpleDateFormat("MM/dd/yy HH:mm:ss").format(told.getDate()) + "] <"
-								+ told.getToNickname() + "> " + told.getMessage();
-						event.getUser().send().message(message);
-					}
-				}
 			}
 			else
 			{
-				event.getChannel().send().message(moduleInfo.getCommands().get("!told"));
+				String toNickname = params[0];
+				tolds = service.getTolds(event.getUser().getNick(), toNickname, event.getBot().getConfiguration()
+						.getServerHostname());
+			}
+
+			if (tolds != null)
+			{
+				for (TellEntity told : tolds)
+				{
+					String message = told.isTold() + " ["
+							+ new SimpleDateFormat("MM/dd/yy HH:mm:ss").format(told.getDate()) + "] <"
+							+ told.getToNickname() + "> " + told.getMessage();
+					event.getUser().send().message(message);
+				}
 			}
 		}
 	}
-	
+
 	@Override
 	public void OnPrivateMessage(PrivateMessageEvent event)
 	{
 		if (isCommand(event.getMessage(), "!tell"))
 		{
-			ArrayList<String> params = new ArrayList<String>(Arrays.asList(event.getMessage().split("\\s")));
-			if (params.size() > 2)
+			String[] params = event.getMessage().replace("!tell", "").trim().split("\\s");
+			if (params.length > 1)
 			{
 				StringBuilder sb = new StringBuilder();
-				for (int i = 2; i < params.size(); i++)
-					sb.append(params.get(i) + " ");
+				for (int i = 1; i < params.length; i++)
+					sb.append(params[i] + " ");
 				String message = sb.toString().trim();
-				String toNickname = params.get(1);
-				
-				if (service.addTell(event.getUser(), toNickname, message, event.getBot().getConfiguration().getServerHostname()))
+				String toNickname = params[0];
+
+				if (service.addTell(event.getUser(), toNickname, message, event.getBot().getConfiguration()
+						.getServerHostname()))
 					event.getUser().send().message(toNickname + " will be told: " + message);
 			}
 			else
@@ -138,29 +144,53 @@ public class Tell extends BaseListener
 		}
 		else if (isCommand(event.getMessage(), "!told"))
 		{
-			String[] params = event.getMessage().split("\\s");
-			OptionParser parser = new OptionParser();
-			parser.acceptsAll(Arrays.asList("nick", "n")).withRequiredArg();
-			parser.acceptsAll(Arrays.asList("limit", "l")).withRequiredArg().ofType(Integer.class);
-			
-			try
+			String[] params = event.getMessage().replaceFirst("!told", "").trim().split("\\s");
+			ArrayList<TellEntity> tolds;
+			if (params[0].equals("-a") || params[0].equals("-all") || params[0].equals(""))
 			{
-				OptionSet options = parser.parse(params);
-				if (options.hasArgument("nick") || options.hasArgument("n"))
+				tolds = service.getAllTolds(event.getUser().getNick(), event.getBot().getConfiguration()
+						.getServerHostname());
+			}
+			else
+			{
+				String toNickname = params[0];
+				tolds = service.getTolds(event.getUser().getNick(), toNickname, event.getBot().getConfiguration()
+						.getServerHostname());
+			}
+
+			if (tolds != null)
+			{
+				for (TellEntity told : tolds)
 				{
-					StringBuilder sb = new StringBuilder();
-					List<?> list = options.nonOptionArguments();
-					for (int i = 1; i < list.size(); i++)
-					{
-						sb.append(list.get(i).toString() + " ");
-					}
-					event.getUser().send().message(sb.toString().trim());
+					String message = told.isTold() + " ["
+							+ new SimpleDateFormat("MM/dd/yy HH:mm:ss").format(told.getDate()) + "] <"
+							+ told.getToNickname() + "> " + told.getMessage();
+					event.getUser().send().message(message);
 				}
 			}
-			catch (Exception e)
-			{
-				
-			}
+
+			// OptionParser parser = new OptionParser();
+			// parser.acceptsAll(Arrays.asList("a", "all"));
+			// parser.acceptsAll(Arrays.asList("n", "nick", "nickname")).withRequiredArg();
+			// parser.acceptsAll(Arrays.asList("limit", "l")).withRequiredArg().ofType(Integer.class).d;
+			// parser.acceptsAll(Arrays.asList("d", "date")).withRequiredArg();
+			//
+			// try
+			// {
+			// OptionSet options = parser.parse(params);
+			// if (options.hasArgument("nick") || options.hasArgument("n"))
+			// {
+			// StringBuilder sb = new StringBuilder();
+			// List<?> list = options.nonOptionArguments();
+			// for (int i = 0; i < list.size(); i++)
+			// sb.append(list.get(i).toString() + " ");
+			// event.getUser().send().message(sb.toString().trim());
+			// }
+			// }
+			// catch (Exception e)
+			// {
+			//
+			// }
 		}
 	}
 }
